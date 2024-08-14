74cda0b30b1263d6da86fb9820ef97dae0ba2a29
Derek Araki-Kurdyla <djak001@bucknell.edu>
2024-08-14 12:57:04 -0400

    player collision

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,10 +13,17 @@ public class Player : MonoBehaviour
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
 
+        float playerRadius = 0.7f;
+        float playerHeight = 2f;
+        float moveDistance = speed * Time.deltaTime;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
+        if (canMove){
+            transform.position += moveDir * moveDistance; //make this frame rate independent
+        }
+
         //if moveDir is equal to Vector3.zero, player is not walking
         isWalking = moveDir != Vector3.zero;
 
-        transform.position += moveDir * speed * Time.deltaTime; //make this frame rate independent
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed); //this handles rotation
     }
 
