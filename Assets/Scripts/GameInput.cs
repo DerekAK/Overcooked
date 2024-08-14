9c35e90d3499d60df100b688aae4f3393d393b3f
Derek Araki-Kurdyla <djak001@bucknell.edu>
2024-08-14 10:46:03 -0400

    new input system

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -3,21 +3,32 @@ using UnityEngine;
 public class GameInput : MonoBehaviour
 {
     // Start is called before the first frame update
-    public Vector2 GetMovementVectorNormalized(){
-        Vector2 inputVector = Vector2.zero; //make sure this is inside Update function otherwise object will never stop moving
 
-        if (Input.GetKey(KeyCode.W)){
-            inputVector.y = 1;
-        }
-        if (Input.GetKey(KeyCode.S)){
-            inputVector.y = -1;
-        }
-        if (Input.GetKey(KeyCode.A)){
-            inputVector.x = -1;
-        }
-        if (Input.GetKey(KeyCode.D)){
-            inputVector.x = 1;
-        }
+    private PlayerInputActions playerInputActions;
+
+    private void Awake(){
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Player.Enable();
+    }
+
+    public Vector2 GetMovementVectorNormalized(){
+        //Vector2 inputVector = Vector2.zero; //make sure this is inside Update function otherwise object will never stop moving
+
+        Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>(); //this is using new input system
+
+
+        // if (Input.GetKey(KeyCode.W)){
+        //     inputVector.y = 1;
+        // }
+        // if (Input.GetKey(KeyCode.S)){
+        //     inputVector.y = -1;
+        // }
+        // if (Input.GetKey(KeyCode.A)){
+        //     inputVector.x = -1;
+        // }
+        // if (Input.GetKey(KeyCode.D)){
+        //     inputVector.x = 1;
+        // }
         return inputVector.normalized;
     }
 }
